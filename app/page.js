b01f4fb67142eb842d28fbd355bd1ec0a384bcb8
devillers <davidevillers@gmail>
2025-04-06 22:09:33 +0200

    accueil

--- app/page.js
+++ app/page.js
@@ -1,24 +1,29 @@
 import Image from "next/image";
+import Card from "../components/Card"; // Adjust the path as necessary
+
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen bg-[#009992] text-white">
+    <div className=" max-w-[1200px] mx-auto relative  text-white">
       {/* Background image (optional) */}
-      <img
+      {/* <img
         src="/fond.jpg"
         alt="Background"
-        className="absolute top-0 left-0 w-full h-full object-cover   z-10"
-      />
+        className="absolute top-0 left-0 w-full h-full object-cover z-10"
+      /> */}
 
       {/* Content wrapper */}
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] relative z-10">
-        <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-          <div className="gap-2">
-            {/* Optional heading or logo */}
-            {/* <div className="text-xl uppercase">Pause  <span className='text-[#bd9254]'>Reflexo</span></div> */}
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 z-10 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 relative z-10">
+      
+        
+         
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
           </div>
-        </main>
+      
       </div>
-    </div>
+
   );
 }
